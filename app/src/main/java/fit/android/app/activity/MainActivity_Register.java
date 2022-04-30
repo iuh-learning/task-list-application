a841742a0f74066310e39b2745b440dc13001e05
iamtun <letuan19431791.iuh@gmail>
2022-04-30 22:24:31 +0700

    feature login and register - done

--- app/src/main/java/fit/android/app/activity/MainActivity_Register.java
+++ app/src/main/java/fit/android/app/activity/MainActivity_Register.java
@@ -1,15 +1,30 @@
 package fit.android.app.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Database;
 
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import fit.android.app.R;
 import fit.android.app.dao.UserDAO;
@@ -19,13 +34,16 @@ import fit.android.app.helper.MessageBoxListener;
 import fit.android.app.model.User;
 
 public class MainActivity_Register extends AppCompatActivity {
-
+    private final String TAG = ">>>>> LOG MESSGAE: ";
     private TextView tvlogIn;
     private EditText txtFullNameRegister;
     private EditText txtEmailRegister;
     private EditText txtPasswordRegister;
     private EditText txtConfirmPasswordRegister;
     private Button btnRegister;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+    private UserDAO userDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +62,7 @@ public class MainActivity_Register extends AppCompatActivity {
         AppDatabase database = AppDatabase.getDatabase(this);
 
         //get userDAO use method
-        UserDAO userDAO = database.userDAO();
+        userDAO = database.userDAO();
 
         //event click text view move log in display
         tvlogIn.setOnClickListener(new View.OnClickListener() {
@@ -66,15 +84,13 @@ public class MainActivity_Register extends AppCompatActivity {
 
                 if(fullName.equals("") || email.equals("") || password.equals("") || confirmPass.equals("")) {
                     Message.showMessage(MainActivity_Register.this,
-                                "Thông báo", "Vui lòng nhập đẩy đủ thông tin!");
+                                "Message", "Please enter all the information!");
                 }else {
                     if(confirmPass.equals(password)) {
-                        User user = new User(email, fullName, password);
-                        userDAO.insert(user);
-                        clearInput();
-                        Message.showMessage(MainActivity_Register.this,"Thông báo", "Tạo tài khoản thành công!");
+                        //call function
+                        registerAndAddAcountToFirebase(email, password, fullName);
                     }else {
-                        Message.showMessage(MainActivity_Register.this,"Thông báo", "Mật khẩu nhập lại phải giống mật khẩu đã nhập!");
+                        Message.showMessage(MainActivity_Register.this,"Message", "Confirm password must equal password!");
                     }
                 }
             }
@@ -86,6 +102,60 @@ public class MainActivity_Register extends AppCompatActivity {
         txtEmailRegister.setText("");
         txtPasswordRegister.setText("");
         txtConfirmPasswordRegister.setText("");
+    }
+
+    private void registerAndAddAcountToFirebase(String email, String password, String fullName) {
+        //init firebase auth
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            try{
+                                //1. if create email - password successfully then create model user
+                                User user = new User(email, fullName, password);
+
+                                //2. insert user into room database
+                                userDAO.insert(user);
+
+                                //3. clear inputs
+                                clearInput();
+
+                                //4. show message notification
+                                Message.showMessage(MainActivity_Register.this,"Message", "Create account success!");
+
+                                //5. save data from database client to firebase
+                                saveDataFromClientToFireBase();
+                            }catch (Exception e) {
+                                Message.showMessage(MainActivity_Register.this,"Message", "Create account fail!");
+                            }
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity_Register.this, "Register fail", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void saveDataFromClientToFireBase() {
+        List<User> users = userDAO.getAll();
+        Map<String, User> mapUsers = new HashMap<>();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        for (User u : users) {
+            mapUsers.put(u.getFullName(), u);
+        }
+
+        try {
+            mDatabase.child("users").setValue(mapUsers);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
